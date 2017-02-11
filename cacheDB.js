29cafc5a56faf71a2df047a5fb639f9bac1db173
Brett Nelson <brettcnelson@gmail.com>
2017-02-10 22:48:00 -0600

    adds html and basic js

---
+++ cacheDB.js
@@ -0,0 +1,14 @@
+
+var DB = function(name) {
+	this.name = name;
+	this.collections = {}
+	// this.collections.z = new Collection()
+}
+
+// DB.prototype
+
+
+
+// var Collection = function(name) {
+// 	name ? this.name = name : 'z'
+// }
